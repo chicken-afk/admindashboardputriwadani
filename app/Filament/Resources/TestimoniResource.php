781550b29f8611622bc1e437e96b5dda4f52de44
chicken-afk <andikayuda628@gmail.com>
2024-05-27 17:12:03 +0700

    testimoni resource

---
+++ app/Filament/Resources/TestimoniResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TestimoniResource\Pages;
+use App\Filament\Resources\TestimoniResource\RelationManagers;
+use App\Models\Testimoni;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TestimoniResource extends Resource
+{
+    protected static ?string $model = Testimoni::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-star';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('testimoni')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('rating')
+                    ->required()
+                    ->numeric()
+                    ->default(5),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('testimoni')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('rating')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTestimonis::route('/'),
+            'create' => Pages\CreateTestimoni::route('/create'),
+            'edit' => Pages\EditTestimoni::route('/{record}/edit'),
+        ];
+    }
+}
--- database/migrations/2021_12_13_153307_create_shop_customers_table.php
+++ database/migrations/2021_12_13_153307_create_shop_customers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration
+return new class () extends Migration
 {
     /**
      * Run the migrations.
@@ -16,7 +16,7 @@ return new class() extends Migration
         Schema::create('shop_customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('photo')->nullable();
             $table->enum('gender', ['male', 'female']);
             $table->string('phone')->nullable();
